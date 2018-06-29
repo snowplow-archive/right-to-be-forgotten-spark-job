701006a633852cc16bf2ef04c390867d2932fb9f
Konstantinos Servis <knservis@gmail.com>
2018-06-29 15:30:17 +0300

    Initial commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,61 @@
+/**
+  * Copyright 2018 Snowplow Analytics
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+import sbt._
+
+object Dependencies {
+
+ val resolutionRepos = Seq(
+   // For Snowplow libs
+    "Snowplow Analytics Maven repo"  at "http://maven.snplow.com/releases/",
+    "user-agent-parser repo"         at "https://clojars.org/repo/",
+    "vfs-s3 project repository"      at "http://dl.bintray.com/content/abashev/vfs-s3"
+ )
+
+  object V {
+    // Java
+    val jsonpath                          = "2.4.0"
+    val vfs2                              = "2.2-1"
+    val vfsS3                             = "2.4.2"
+    // Scala
+    val spark                             = "2.3.1"
+    val commonEnrich                      = "0.35.0"
+    val hadoopAWS                         = "2.7.3"
+    val snowplowScalaAnalyticsSdkVersion  = "0.3.1"
+    val json4sVersion                     = "3.2.11"
+    val igluScalaClient                   = "0.5.0"
+    val scallop                           = "3.1.2"
+    // Scala (test only)
+    val scalatest                         = "3.0.5"
+  }
+
+  object Libraries {
+    // Java
+    val jsonPath                   = "com.jayway.jsonpath"   %  "json-path"                     % V.jsonpath
+    val vfs2                       = "com.github.abashev"    %  "commons-vfs2"                  % V.vfs2
+    val vfsS3                      = "com.github"            %  "vfs-s3"                        % V.vfsS3
+    // Scala
+    val sparkCore                  = "org.apache.spark"      %% "spark-core"                    % V.spark                            % "provided"
+    val sparkSQL                   = "org.apache.spark"      %% "spark-sql"                     % V.spark                            % "provided"
+    val hadoopAWS                  = "org.apache.hadoop"     % "hadoop-aws"                     % V.hadoopAWS                        % "provided"
+    val snowplowScalaAnalyticsSdk  = "com.snowplowanalytics" %% "snowplow-scala-analytics-sdk"  % V.snowplowScalaAnalyticsSdkVersion
+    val igluScalaClient            = "com.snowplowanalytics" %% "iglu-scala-client"             % V.igluScalaClient
+    val json4s                     = "org.json4s"            %% "json4s-jackson"                % V.json4sVersion
+    val scallop                    = "org.rogach"            %% "scallop"                       % V.scallop
+    // Test
+    val scalatest                  = "org.scalatest"         %% "scalatest"                     % V.scalatest                        % "test"
+  }
+}
---
+++ src/main/scala/com/snowplowanalytics/righttobeforgotten/main.scala
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2018 Snowplow Analytics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.snowplowanalytics
+package righttobeforgotten
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.rogach.scallop.ScallopConf
+
+object Main {
+
+  /**
+   * Main entry point
+   * @param args arguments provided
+   */
+  def main(args: Array[String]): Unit = {
+    val appArgsValidated = new R2FConfArguments(args)
+    val r2fConfig        = ValidatedR2FJobConfiguration(appArgsValidated)
+    val sparkConf = new SparkConf()
+      .setAppName("snowplow-r2f-job")
+    val spark: SparkSession =
+      SparkSession
+        .builder()
+        .config(sparkConf)
+        .getOrCreate()
+    val job = new RightToBeForgottenSparkJob(r2fConfig)
+    job.run(spark)
+  }
+}
+
+class R2FConfArguments(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val removalCriteria = opt[String](name = "removal-criteria", required = true)
+  val inputDirectory  = opt[String](name = "input-directory", required  = true)
+  val matchingOutputDirectory =
+    opt[String](name = "matching-output-directory", required = false)
+  val nonMatchingOuputDirectory =
+    opt[String](name = "non-matching-output-directory", required = true)
+  val maximumMatchingProportion =
+    opt[Double](name = "maximum-matching-proportion", required = true)
+  verify()
+}
+
+case class ValidatedR2FJobConfiguration(arguments: R2FConfArguments) {
+  val inputDirectory                 = arguments.inputDirectory()
+  val removalCriteriaFile            = arguments.removalCriteria()
+  val outputMatchingDataDirectory    = arguments.matchingOutputDirectory.toOption
+  val outputNonMatchingDataDirectory = arguments.nonMatchingOuputDirectory()
+  val maximumMatchingProportion      = arguments.maximumMatchingProportion()
+  val removalCriteriaLines           = ValueFilters.getRemovalCriteriaLines(removalCriteriaFile)
+  val valueFilters = ValueFilters
+    .createFiltersFromRemovalCriteria(removalCriteriaLines)
+    .fold(errorList => throw new InvalidDataInRightToBeForgottenConfiguration(errorList.mkString("\n")), vF => vF)
+}
